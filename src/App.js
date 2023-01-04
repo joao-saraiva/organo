c8e487d94f056c5d912d9fedb1ecb483f8659c88
Joao Victor Saraiva <joaovsaraivasousa@gmail.com>
2023-01-04 20:57:07 -0300

    creatgin team section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Banner from "./components/Banner";
 import Form from "./components/Form";
+import Team from "./components/Team";
 function App() {
 
   const [employees, setEmployee] = useState([]);
@@ -14,6 +15,10 @@ function App() {
     <div className="App">
       <Banner />
       <Form onEmployeeRegistered={onEmployeeRegistered}/>
+      <Team name="Programação"/>
+      <Team name="Front-End"/>
+      <Team name="Data Science" />
+      
     </div>
   );
 }
