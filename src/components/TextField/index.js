89b923e680d221bd2ec2e09d4e62a950ec01981f
Joao Victor Saraiva <joaovsaraivasousa@gmail.com>
2023-01-04 19:45:42 -0300

    creating onchange event for form input, hardcoded value

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,9 +1,16 @@
 import "./TextField.css";
 const TextField = (props) => {
+
+  let textFieldValue = 'Gustavinho';
+
+  const afterType = (event) => {
+    textFieldValue = event.target.value
+  }
+
   return (
     <div className="text-field">
       <label>{props.label}</label>
-      <input required={props.required} placeholder={props.placeholder} />
+      <input value={textFieldValue} onChange={afterType} required={props.required} placeholder={props.placeholder} />
     </div>
   );
 };
