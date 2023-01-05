d207fe17d5c12b3dd420ba09f5dd7da4ab35463a
joao <joaovsaraivasousa@gmail.com>
2023-01-05 20:40:32 -0300

    creating employee card

---
+++ src/components/Employee/index.js
@@ -0,0 +1,17 @@
+import "./Employee.css";
+
+const Employee = () => {
+  return (
+    <div className="employee">
+      <div className="header">
+        <img src="https://github.com/joao-saraiva.png" alt="João Saraiva"></img>
+      </div>
+      <div className="footer">
+        <h4>João</h4>
+        <h5>Programador</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Employee;
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,9 +1,11 @@
+import Employee from "../Employee";
 import "./Team.css";
 
 const Team = (props) => {
   return (
     <section className="team" style={{ backgroundColor: props.secundaryColor }}>
       <h3 style={{ borderBottomColor: props.primaryColor }}>{props.name}</h3>
+      <Employee />
     </section>
   );
 };
